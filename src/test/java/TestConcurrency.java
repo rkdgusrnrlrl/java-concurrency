90ff846a298cd8971b8abc4e88e96c3678ee7e49
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-05-05 19:59:58 +0900

    feat:base class and and make fail test

---
+++ src/test/java/TestConcurrency.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
+
+public class TestConcurrency {
+
+    @Test
+    public void testIsOk() {
+        fail("fail is  ok");
+    }
+}
