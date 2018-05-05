4327253f3aaf08af7f9dd0acbf591e6cfbffe72a
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-05-05 20:36:54 +0900

    feat:print thread name but not work synchronize

--- src/main/java/ConrrencyTutorial.java
+++ src/main/java/ConrrencyTutorial.java
@@ -1,2 +1,15 @@
 public class ConrrencyTutorial {
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Hello " + threadName);
+        };
+
+        task.run();
+
+        Thread thread = new Thread(task);
+        thread.start();
+
+        System.out.println("Done !!");
+    }
 }
