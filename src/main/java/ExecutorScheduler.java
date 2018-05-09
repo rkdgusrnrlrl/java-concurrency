a9237c005d344af546ea0931f0c94e3a495b977a
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-05-09 20:14:14 +0900

    feat:test scheduler thread pool

---
+++ src/main/java/ExecutorScheduler.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorScheduler {
+    public static void main(String[] args) {
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        Runnable task = () -> System.out.println("Scheduling : " + System.nanoTime());
+
+        int initialDelay = 0;
+        int period = 1;
+
+        executor.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.SECONDS);
+    }
+}
